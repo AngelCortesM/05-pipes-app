3a3c18e7ad02a62f976aa8f5c512a49dfda51fc4
AngelCortesM <joseangelcortesmontoya@gmail.com>
2025-06-10 14:49:06 -0500

    feat: configure application locale to Spanish and improve code structure

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,22 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  LOCALE_ID,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import localEs from '@angular/common/locales/es-CO';
 import { routes } from './app.routes';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localEs, 'es');
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    {
+      provide: LOCALE_ID,
+      useValue: 'es',
+    },
+  ],
 };
