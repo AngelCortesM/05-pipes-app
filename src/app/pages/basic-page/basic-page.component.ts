5fabf07c612d9bc0ca4c81948f6e6368c5b70be6
AngelCortesM <joseangelcortesmontoya@gmail.com>
2025-06-10 14:42:14 -0500

    feat: add DateCase pipe examples and update component to include DatePipe

--- src/app/pages/basic-page/basic-page.component.ts
+++ src/app/pages/basic-page/basic-page.component.ts
@@ -1,9 +1,14 @@
-import { LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
-import { Component, signal } from '@angular/core';
+import {
+  DatePipe,
+  LowerCasePipe,
+  TitleCasePipe,
+  UpperCasePipe,
+} from '@angular/common';
+import { Component, effect, signal } from '@angular/core';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe],
+  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe, DatePipe],
   templateUrl: './basic-page.component.html',
   styleUrl: './basic-page.component.css',
 })
@@ -13,4 +18,13 @@ export default class BasicPageComponent {
   fullName = signal('jOse mOntoya');
 
   customDate = signal(new Date());
+  tickingDateEffect = effect((onCleanup) => {
+    const interval = setInterval(() => {
+      this.customDate.set(new Date());
+    }, 1000);
+
+    onCleanup(() => {
+      clearInterval(interval);
+    });
+  });
 }
